414fdb2b50b4efe4f96086032777920db7545652
Sibasis-Malla <sibasisml@gmail.com>
2024-05-31 23:19:30 +0530

    feat:admin page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,34 @@
-import { EthProvider } from "./contexts/EthContext";
-import Intro from "./components/Intro/";
-import Setup from "./components/Setup";
-import Demo from "./components/Demo";
-import Footer from "./components/Footer";
+import React, { useEffect, useContext } from 'react';
+import { Route, Routes } from 'react-router-dom';
 
-function App() {
+// Import your components for different pages
+import Web3Context from './contexts';
+
+import HomePage from './pages/HomePage';
+import StakePage from './pages/StakePage';
+import AdminPage from './pages/AdminPage';
+import StorageProvider from './pages/StorageProvider';
+
+const App = () => {
+  window.ethereum &&
+    window.ethereum.on('accountsChanged', function (accounts) {
+      setTimeout(window.location.reload(false), 1000);
+    });
+
+  //CheckIfWallet is Connectd
+  const { checkIfWalletIsConnected } = useContext(Web3Context);
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
   return (
-    <EthProvider>
-      <div id="App">
-        <div className="container">
-          <Intro />
-          <hr />
-          <Setup />
-          <hr />
-          <Demo />
-          <hr />
-          <Footer />
-        </div>
-      </div>
-    </EthProvider>
+    <Routes>
+      <Route exact path="/" element={<HomePage />} />
+      <Route exact path="/stake" element={<StakePage />} />
+      <Route exact path="/admin" element={<AdminPage />} />
+      <Route exact path="/sp" element={<StorageProvider />} />
+    </Routes>
   );
-}
+};
 
 export default App;
