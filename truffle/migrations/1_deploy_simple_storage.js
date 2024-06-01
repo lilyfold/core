9d69049b30b5fb962c7e8efcab323a30cb4308ab
Sibasis-Malla <sibasisml@gmail.com>
2024-06-01 23:04:12 +0530

    feat:minor bug fix

--- truffle/migrations/1_deploy_simple_storage.js
+++ truffle/migrations/1_deploy_simple_storage.js
@@ -2,8 +2,8 @@
 const Pool = artifacts.require("Pool");
 const StorageProvider = artifacts.require("StorageProviderContract");
 module.exports = function (deployer) {
-  // deployer.deploy(StorageProvider);
-  deployer.deploy(Pool,"0xaC26a4Ab9cF2A8c5DBaB6fb4351ec0F4b07356c4","Fold Token","dFIL");
+  deployer.deploy(StorageProvider);
+  // deployer.deploy(Pool,"0xaC26a4Ab9cF2A8c5DBaB6fb4351ec0F4b07356c4","Fold Token","dFIL");
 
   // deployer.deploy(SimpleStorage);
 };
