e3d2924266f96378c4e592264bacf1c111e61bce
Sibasis-Malla <sibasisml@gmail.com>
2024-06-01 17:57:07 +0530

    feat:Contract Utils

--- truffle/migrations/1_deploy_simple_storage.js
+++ truffle/migrations/1_deploy_simple_storage.js
@@ -1,5 +1,8 @@
 const SimpleStorage = artifacts.require("SimpleStorage");
-
+const Pool = artifacts.require("Pool");
+const StorageProvider = artifacts.require("StorageProviderContract");
 module.exports = function (deployer) {
+    deployer.deploy(Pool,"0x65E4e8D7bd50191abFee6E5BcDc4d16DDfE9975E","Fold Token","deFIL");
+
   deployer.deploy(SimpleStorage);
 };
