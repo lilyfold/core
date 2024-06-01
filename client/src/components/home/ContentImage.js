6497e8cbdaf501cf3d7dd80e8663116171dcf143
Sibasis-Malla <sibasisml@gmail.com>
2024-06-01 16:25:40 +0530

    feat:landing page content

---
+++ client/src/components/home/ContentImage.js
@@ -0,0 +1,64 @@
+import { SectionContainer } from '../commons/SectionContainer';
+import { v4 as uuid } from 'uuid';
+
+const ContentImageData = [
+  {
+    id: uuid(),
+    title: 'Stake FIL and rewards',
+    content: 'Got FIL? stake and watch your FIL grow.',
+    align: 'right',
+    image: '',
+  },
+  {
+    id: uuid(),
+    title: 'Loans',
+    content:
+      'Got Storage but cant stake? Request for funds to participate in the network !',
+    align: 'left',
+    image: '',
+  },
+  {
+    id: uuid(),
+    title: 'Reputation',
+    content:
+      'Worried about collateral? Know what the network thinks of you.',
+    align: 'right',
+    image: '',
+  },
+];
+
+export const ContentImage = () => {
+  return (
+    <SectionContainer className="mt-16 flex w-10/12 flex-col items-center justify-center space-y-16">
+      {ContentImageData.map((item) => (
+        <div id={item.id} key={item.id} className="grid gap-y-8 md:grid-cols-2">
+          <div
+            className={`rounded-3xl ${
+              item.align === 'left' ? 'md:order-1' : ''
+            }`}
+          >
+            <img
+              src={item.image}
+              objectFit="cover"
+              alt="Process Banner 1"
+              className="offset-y-0 offset-x-8 blur-16 w-full drop-shadow-xl"
+            />
+          </div>
+          <div
+            className={`flex items-start justify-center flex-col ${
+              item.align === 'left'
+                ? 'ml-auto md:pr-16 lg:pr-24 xl:pr-32'
+                : 'mr-auto md:pl-16 lg:pl-24  xl:pl-32'
+            } content my-auto text-black/60`}
+          >
+            <h3 className="md:h3 mb-2 text-[30px] font-semibold text-white">
+              {item.title}
+            </h3>
+            <div className="mt-0 w-36 border-b-4 border-secondary-500"></div>
+            <p className="text-secondary-200 !text-[15px] mt-2 opacity-60">{item.content}</p>
+          </div>
+        </div>
+      ))}
+    </SectionContainer>
+  );
+};
---
+++ client/src/components/home/HomeBanner.js
@@ -0,0 +1,58 @@
+import { faArrowRight, faDatabase } from '@fortawesome/free-solid-svg-icons';
+import { BadgeGroup, BadgeIcon, BadgeMessage } from '../home/Badge';
+import { ButtonGroup } from '../Button';
+import { Content } from '../home/Content';
+import { MotionBTTContainer } from '../commons/MotionBTTContainer';
+import { SectionContainer } from '../commons/SectionContainer';
+import { PageTitle } from '../commons/PageTitle';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from 'react-router-dom';
+
+export const HomeBanner = () => {
+  return (
+    <SectionContainer className="flex items-center justify-center py-16">
+      <SectionContainer className="wrap wrap-px z-10 flex flex-col items-center justify-center">
+        {/* Appear Second */}
+        <MotionBTTContainer transition={{ delay: 0.4, duration: 0.5 }}>
+          <PageTitle className="mx-auto text-center text-white" type="heavy">
+      Empowering Storage Providers
+        </PageTitle>
+        </MotionBTTContainer>
+        {/* Appear Third */}
+        <MotionBTTContainer transition={{ delay: 0.6, duration: 0.5 }}>
+          <div className="mb-16 mt-6 flex items-center justify-center text-center">
+            <div className="block">
+              <Link
+                to="/stake"
+                className="mr-3 inline-flex w-full cursor-pointer items-center justify-center gap-3 whitespace-nowrap rounded-lg bg-primary-500 px-8 py-2  text-xl font-semibold text-black transition-colors duration-300 hover:bg-secondary-500 md:w-auto"
+              >
+                Deposit FIL and get rewarded !
+                <FontAwesomeIcon icon={faArrowRight} />
+              </Link>
+            </div>
+            <div className="block">
+              <Link
+                to="/sp"
+                className="ml-3 inline-flex w-full cursor-pointer items-center justify-center gap-3 whitespace-nowrap rounded-lg bg-primary-500 px-8 py-2  text-xl font-semibold text-black transition-colors duration-300 hover:bg-secondary-500 md:w-auto"
+              >
+                Storage Provider? Explore
+                <FontAwesomeIcon icon={faArrowRight} />
+              </Link>
+            </div>
+          </div>
+        </MotionBTTContainer>
+        {/* Appear Fourth */}
+        <MotionBTTContainer transition={{ delay: 0.8, duration: 0.5 }}>
+          <div className="page-banner--image">
+            <img
+              src=""
+              alt="Page Banner"
+              objectFit="cover"
+              className="mx-auto h-[630px] w-[1024px]"
+            />
+          </div>
+        </MotionBTTContainer>
+      </SectionContainer>
+    </SectionContainer>
+  );
+};
